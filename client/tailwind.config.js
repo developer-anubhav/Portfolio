20176b672d69449d00cbc6af25d650bd433a96df
Anubhav <anubhavfordev24@gmail.com>
2026-04-02 17:51:25 +0530

    Updates after Navbar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fade-in 1s ease-out forwards',
+        'slide-up': 'slide-up 1s ease-out forwards',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        'slide-up': {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        }
+      }
+    },
   },
   plugins: [],
 }
